bbed7c6da8b886b590a3245265c27f124599b5ea
mokurin000 <mokurin000@users.noreply.github.com>
2026-07-02 01:03:55 +0000

    deploy: 15a55090ae8a14930e2c1b73c3a9779ec34fc932

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4c45df65.js',
+                path_to_root + 'searchindex-e7c3d3d8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
